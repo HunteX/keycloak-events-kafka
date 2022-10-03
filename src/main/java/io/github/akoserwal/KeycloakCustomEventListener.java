693d7a0514653637030cb35dcbdfc76915c2b2fe
Andrey Alyonin <huntex@yandex.ru>
2022-10-03 21:23:33 +0300

    upd

--- src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
+++ src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
@@ -8,14 +8,15 @@ public class KeycloakCustomEventListener implements EventListenerProvider {
 
 	@Override
 	public void onEvent(Event event) {
-			System.out.println("Event:-"+event.getUserId());
-			Producer.publishEvent(event.getType().toString(), event.getUserId());
+//			System.out.println("Event:-"+event.getUserId());
+//			Producer.publishEvent(event.getType().toString(), event.getUserId());
 	}
 
 	@Override
 	public void onEvent(AdminEvent adminEvent, boolean b) {
 			System.out.println("Admin Event:-"+adminEvent.getResourceType().name());
-			Producer.publishEvent(adminEvent.getOperationType().toString(), adminEvent.getAuthDetails().getUserId());
+//			Producer.publishEvent(adminEvent.getOperationType().toString(), adminEvent.getAuthDetails().getUserId());
+			Producer.publishEvent("KEYCLOAK", adminEvent.getAuthDetails().getUserId());
 	}
 
 	@Override
