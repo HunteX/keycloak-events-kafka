961d3b24993adc5831133e0fdc28748b898bc83f
Andrey Alyonin <huntex@yandex.ru>
2022-10-10 10:55:57 +0300

    up

--- src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
+++ src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
@@ -4,23 +4,43 @@ import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.admin.AdminEvent;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.jboss.logging.Logger;
+
 public class KeycloakCustomEventListener implements EventListenerProvider {
+    ObjectMapper eventMapper = new ObjectMapper();
 
-	@Override
-	public void onEvent(Event event) {
-//			System.out.println("Event:-"+event.getUserId());
-//			Producer.publishEvent(event.getType().toString(), event.getUserId());
-	}
+    private static Logger logger = Logger.getLogger(KeycloakCustomEventListener.class);
 
-	@Override
-	public void onEvent(AdminEvent adminEvent, boolean b) {
-			System.out.println("Admin Event:-"+adminEvent.getResourceType().name());
-//			Producer.publishEvent(adminEvent.getOperationType().toString(), adminEvent.getAuthDetails().getUserId());
-			Producer.publishEvent("KEYCLOAK", adminEvent.getAuthDetails().getUserId());
-	}
+    public KeycloakCustomEventListener() {
 
-	@Override
-	public void close() {
+    }
 
-	}
+    @Override
+    public void onEvent(AdminEvent adminEvent, boolean includeRepresentation) {
+        // System.out.println("Admin Event:-" + adminEvent.getResourceType().name());
+        // Producer.publishEvent(adminEvent.getOperationType().toString(), adminEvent.getAuthDetails().getUserId());
+
+        // TODO: add config support https://github.com/athenacykes/keycloak-kafka-eventlistener
+        //		KafkaEventListenerConfig kafkaConfig = new KafkaEventListenerConfig();
+        //		Properties properties = kafkaConfig.getProperties();
+        // String topicName = properties.getProperty(KafkaEventListenerConfig.KAFKA_CONFIG_TOPIC_NAME_ADMIN);
+
+        ObjectMapper eventMapper = new ObjectMapper();
+        JsonNode eventJson = eventMapper.convertValue(adminEvent, JsonNode.class);
+
+        Producer.publishEvent("KEYCLOAK", eventJson.toString());
+    }
+
+    @Override
+    public void onEvent(Event event) {
+        // ignore
+    }
+
+    @Override
+    public void close() {
+        // ignore
+    }
 }
